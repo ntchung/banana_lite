bbd557eca70dc9c806b0acb47d09eb2c2508d7f2
Nguyen Truong Chung <fowlwarrior@hotmail.com>
2014-07-25 00:55:34 +0700

    + starting vserv

--- make_src.bat
+++ make_src.bat
@@ -21,11 +21,11 @@ for %%f in (src\*.java) do %CPP_BIN% -P -I src %%f _tempsrc\%%~nf.java
 echo Compiling
 
 rem %JAVA_BIN%\javac -target 1.1 -bootclasspath %WTK_HOME%\lib\cldcapi11.jar;%WTK_HOME%\lib\midpapi20.jar -classpath %NOKIA_SDK_HOME%\lib\nokiaui.jar _tempsrc\*.java -d _temp
-%JAVA_BIN%\javac -target 1.4 -source 1.4 -classpath %NOKIA_SDK_HOME%\lib\nokiaui.jar -bootclasspath %WTK_HOME%\lib\cldcapi11.jar;%WTK_HOME%\lib\midpapi20.jar _tempsrc\*.java -d _temp
+%JAVA_BIN%\javac -target 1.4 -source 1.4 -classpath %NOKIA_SDK_HOME%\lib\nokiaui.jar;tools\VservAdEngine.jar -bootclasspath %WTK_HOME%\lib\cldcapi11.jar;%WTK_HOME%\lib\midpapi20.jar _tempsrc\*.java -d _temp
 
 echo Preverifying
 
-%WTK_BIN%\preverify1.1 -classpath %WTK_HOME%\lib\cldcapi10.jar;%WTK_HOME%\lib\midpapi20.jar -d _temp _temp
+%WTK_BIN%\preverify1.1 -classpath %WTK_HOME%\lib\cldcapi10.jar;%WTK_HOME%\lib\midpapi20.jar;tools\VservAdEngine.jar -d _temp _temp
 
 echo Jaring
 %JAVA_BIN%\jar cfm _release\%GAME_NAME%.jar MANIFEST.MF -C _temp .
